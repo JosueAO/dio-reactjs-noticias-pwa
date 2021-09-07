82bf309865a12cfa7c9b9ec0de0b2a9b00547659
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2021-09-07 11:09:00 -0300

    API e dependecias concluidas

--- pwa-news-dio/src/App.js
+++ pwa-news-dio/src/App.js
@@ -1,14 +1,23 @@
-//import { Router } from 'react-router';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css';
+import Home from './containers/Home'
+import Post from './containers/Post'
 
 function App() {
   return (
     <main>
       <section>
-        <Router path="/">
+        <Router>
           <div>
-            post
+            <Switch>
+              <Route path="/:subject/:id">
+                <Post />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
           </div>
         </Router>
       </section>
